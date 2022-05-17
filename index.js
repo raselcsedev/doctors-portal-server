b8ccade12e66e2c2028dd89f9ff89ce7dbcc794e
raselcsedev <raselcse.dev@gmail.com>
2022-05-17 13:25:59 +0600

    find available time slots for a day

--- index.js
+++ index.js
@@ -17,12 +17,50 @@ async function run (){
     try{
         await client.connect();
         const servicesCollection = client.db('doctors_portal').collection('services');
+        const bookingCollection = client.db('doctors_portal').collection('booking');
+
+
         app.get('/service', async(req,res)=>{
             const query = {};
             const cursor = servicesCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        });
+
+        app.get('/available', async(req, res)=>{
+          const date = req.query.date;
+
+          //get all services
+          const services = await servicesCollection.find().toArray();
+
+          //get the booking of that day
+          const query = {date: date};
+          const bookings = await bookingCollection.find(query).toArray();
+
+          //for each service, find bookings for that service
+          services.forEach(service =>{
+            const serviceBookings = bookings.filter(book =>book.treatment === service.name);
+            const bookedSlots = serviceBookings.map(book => book.slot);
+            const available = service.slots.filter(slot => !bookedSlots.includes(slot));
+            service.slots = available;
+          })
+
+
+          res.send(services);
+
         })
+
+        app.post('/booking', async(req, res)=>{
+          const booking = req.body;
+          const query = {treatment: booking.treatment, date:booking.date, patient:booking.patient};
+          const exists = await bookingCollection.findOne(query);
+          if(exists){
+            return res.send({success: false, booking: exists})
+          }
+          const result = await bookingCollection.insertOne(booking);
+          return res.send({success: true,  result});
+        })
+
         console.log('Database connected');
     }
     finally{
